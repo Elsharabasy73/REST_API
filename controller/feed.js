41d6ebb8a024fc8d6e774cbd8bba77497c67f639
Elsharabasy73 <abdo.make73@gmail.com>
2024-02-07 21:30:13 +0200

    [add]Play 378. Storing Posts in the Database

--- controller/feed.js
+++ controller/feed.js
@@ -1,13 +1,15 @@
+const Post = require("../models/post");
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
       {
-        _id:'1',
+        _id: "1",
         title: "First Post",
         content: "This is my first post.",
-        imageUrl:'images/duck.jpeg',
-        creator:{
-          name:"ElSharabasy"
+        imageUrl: "images/duck.jpeg",
+        creator: {
+          name: "ElSharabasy",
         },
         createdAt: new Date(),
       },
@@ -17,16 +19,19 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
   const { title, content } = req.body;
 
-  res.status(201).json({
-    message: "Post Created successfully",
-    post: {
-      _id:new Date().toISOString(),
-      title: title,
-      content: content,
-      creator:{
-        name:'temp-Elsharabasy'
-      },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "/images/duck.jpeg",
+    creator: { name: "temp-Elsharabasy" },
   });
+  post
+    .save()
+    .then((resultedPost) => {
+      res.status(201).json({
+        message: "Post Created successfully",
+        post: resultedPost,
+      });
+    })
+    .catch((err) => console.log(err));
 };

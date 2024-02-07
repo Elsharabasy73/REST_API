1c8353b75856c811aeb72fd0e6f4e2debbc371a1
Elsharabasy73 <abdo.make73@gmail.com>
2024-02-07 21:02:44 +0200

    [add]383 Setting Up a Post Model

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const body_parser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRouter = require("./router/feed");
+
+const MONGODB_URL =
+  // "mongodb+srv://abdomake73:xlsgzIvu2CYeOTrg@cluster0.vclsggt.mongodb.net/shop";
+  "mongodb+srv://abdomake73:xlsgzIvu2CYeOTrg@cluster0.vclsggt.mongodb.net/messages?retryWrites=true&w=majority";
+// "mongodb://localhost:27017/";
 
 const app = express();
 
@@ -13,11 +19,17 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", 'Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
 app.use("/feed", feedRouter);
 
-app.listen(8080);
-console.log("listenning");
+mongoose
+  .connect(MONGODB_URL)
+  .then((res) => {
+    console.log("db connected");
+    app.listen(8080);
+    console.log("listinning on port 8080");
+  })
+  .catch();

fabd28838ccc41e70b81ecd958ebd95a4ae7307d
Elsharabasy73 <abdo.make73@gmail.com>
2024-02-08 23:40:48 +0200

    [add]385. Adding Pagination

--- controller/feed.js
+++ controller/feed.js
@@ -5,11 +5,22 @@ const path = require("path");
 const Post = require("../models/post");
 
 exports.getPosts = (req, res, next) => {
-  Post.find()
+  const currentPage = req.query.page || 1;
+  const perPage = 2;
+  let totalItems;
+  Post.countDocuments()
+    .then((numOfPosts) => {
+      totalItems = numOfPosts;
+      return Post.find()
+        .skip((currentPage - 1) * perPage)
+        .limit(perPage);
+    })
     .then((posts) => {
-      res
-        .status(200)
-        .json({ massage: "Fetched posts successfully.", posts: posts });
+      res.status(200).json({
+        massage: "Fetched posts successfully.",
+        posts: posts,
+        totalItems: totalItems,
+      });
     })
     .catch((err) => {
       if (!err.statusCode) err.statusCode = 500;
